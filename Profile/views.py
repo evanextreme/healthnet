61a1475f8f31bdf0d0da8e762f541a45e8d8b6fe
jas7229 <jas7229@ba42aba2-8e26-4a4c-85cc-ac5429f0f81b>
2017-03-02 22:12:36 +0000

    attempted to get login page running but django cant find login template
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@19 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- Profile/views.py
+++ Profile/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def login(request):
+	return HttpResponse("Hello, you're at the login page")
 
 # Create your views here.
