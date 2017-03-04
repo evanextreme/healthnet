fdacb636ec9f148e774a21d093f728b6a865fdb9
Evan Hirsh <evanx11@gmail.com>
2017-03-04 17:45:08 +0000

    Removed HNet, migrated models to HealthNet admin
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@21 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- HealthNet/settings.py
+++ HealthNet/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'eventlog',
+    'schedule',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
+                "django.core.context_processors.request",
             ],
         },
     },
