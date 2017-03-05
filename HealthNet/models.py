f61349f302c4c8388cca7727c3981dbc70bcc109
s261-08d <s261-08d@ba42aba2-8e26-4a4c-85cc-ac5429f0f81b>
2017-03-05 22:56:03 +0000

    Models fixed. added related_name args and moved models so they are defined by the time they are used
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@32 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- HealthNet/models.py
+++ HealthNet/models.py
@@ -5,6 +5,14 @@ import datetime
 # Create your models here.
 
 
+
+class Hospital(models.Model):
+    hospitals = models.Manager()
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+
+
+
 class Person(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -17,6 +25,21 @@ class Person(models.Model):
         return name
 
 
+class Nurse(Person):
+    nurses = models.Manager()
+    employment_date = models.DateTimeField('Employment Date')
+    employee_id = models.IntegerField()
+
+    current_hospital_assignment = models.ForeignKey(
+        Hospital,
+        on_delete=models.CASCADE
+    )
+
+
+class Doctor(Nurse):
+    doctors = models.Manager()
+
+
 class Patient(Person):
     patients = models.Manager()
     #username = models.CharField(max_length=20)
@@ -25,83 +48,56 @@ class Patient(Person):
     height = models.IntegerField()
     weight = models.IntegerField()
 
-    #doctor_assignment = models.ForeignKey(
-    #    Doctor,
-    #    on_delete=models.CASCADE
-    #)
-    #nurse_assignment = models.ForeignKey(
-    #    Nurse,
-    #    on_delete=models.CASCADE
-    #)
-    #current_hospital_assignment = models.ForeignKey(
-    #    Hospital,
-    #    on_delete=models.CASCADE
-    #)
-
-    def get_doctor(doctor_request):
-        return
-    #stubbed method for requesting a doctor_assignment
-
-class Nurse(Person):
-    nurses = models.Manager()
-    employment_date = models.DateTimeField('Employment Date')
-    employee_id = models.IntegerField()
-
-    patient_list = models.ForeignKey(
-        Patient,
+    doctor_assignment = models.ForeignKey(
+        Doctor,
+        on_delete=models.CASCADE,
+        related_name ="doctor"
+    )
+    nurse_assignment = models.ForeignKey(
+        Nurse,
+        on_delete=models.CASCADE,
+        related_name ="nurse"
+    )
+    current_hospital_assignment = models.ForeignKey(
+        Hospital,
         on_delete=models.CASCADE
     )
-    #current_hospital_assignment = models.ForeignKey(
-    #    Hospital,
-    #    on_delete=models.CASCADE
-    #)
-
-    def create_appointment(patient, time, doctor):
-        return
-    #stubbed method for creating appts w/o test capabilities
-
-class Doctor(Nurse):
-    doctors = models.Manager()
-
-    def create_appointment_test(patient, time, test_type):
-        return
-    #stubbed method for creating appts w/ test capabilities
-
-class Hospital(models.Model):
-    hospitals = models.Manager()
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=200)
 
 
+
+
+
+
+
+
+class Appointment(models.Model):
 class Appointment(models.Model):
     appointments = models.Manager()
     patient = models.ForeignKey(
         Patient,
         on_delete=models.CASCADE
     )
-    #doctor = models.ForeignKey(
-    #    Doctor,
-    #    on_delete=models.CASCADE
-    #)
-    #nurse = models.ForeignKey(
-    #    Nurse,
-    #    on_delete=models.CASCADE
-    #)
+    doctor = models.ForeignKey(
+        Doctor,
+        on_delete=models.CASCADE,
+        related_name="attending_doctor"
+    )
+    nurse = models.ForeignKey(
+        Nurse,
+        on_delete=models.CASCADE,
+        related_name ="attending_nurse"
+    )
     notes = models.TextField()
 
-    #appt_type_choices = (
-    #    ('test', 'Test'),
-    #    ('surgery', 'Surgery')
-    #    ('checkup', 'Check Up'),
-    #    (None, 'Misc')
-    #)
+    appt_type_choices = (
+        ('test', 'Test'),
+        ('surgery', 'Surgery'),
+        ('checkup', 'Check Up'),
+        (None, 'Misc')
+    )
 
-    #appointment_type = models.CharField(
-    #    max_length=10,
-    #    choices = appt_type_choices,
-    #    default = None
-    #)
-
-    def modify(self):
-        return
-    #stubbed method to modify appointments
+    appointment_type = models.CharField(
+        max_length=10,
+        choices = appt_type_choices,
+        default = None
+    )
--- HealthNet/urls.py
+++ HealthNet/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    #url(r'^login/$', auth_views.login, name='login'),
+    url(r'^login/$', auth_views.login, name='login'),
     #url(r'^logout/$', auth_views.logout, name='logout'),
 	url(r'^', include('Profile.urls')),
+    url(r'^appt/', include('appointments.urls')),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include ('Calendar.urls')),
