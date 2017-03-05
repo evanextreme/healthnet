eb600c645ef018c68429f2793069317e2ee5d6ca
s261-08d <s261-08d@ba42aba2-8e26-4a4c-85cc-ac5429f0f81b>
2017-03-05 22:56:03 +0000

    Models fixed. added related_name args and moved models so they are defined by the time they are used
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@32 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- HealthNet/settings.py
+++ HealthNet/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'appointments',
     'eventlog',
     'schedule',
+    'appointments',
+    'HealthNet'
 ]
 
 MIDDLEWARE = [
--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from HealthNet import models
 
-# Create your views here.
+
+
+def new_appt(request):
+    return HttpResponse("new_appt.html")
