d7ea9f7639ba49359198a7c5fbb14a933c899328
s261-08d <s261-08d@ba42aba2-8e26-4a4c-85cc-ac5429f0f81b>
2017-03-05 21:35:25 +0000

    scheduler first implementation -evan
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@30 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- HealthNet/settings.py
+++ HealthNet/settings.py
@@ -66,7 +66,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
-                #"django.core.context_processors.request",
             ],
         },
     },
--- HealthNet/urls.py
+++ HealthNet/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     #url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 	url(r'^', include('Profile.urls')),
-	url(r'^', include('Calendar.urls')),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include ('Calendar.urls')),
 ]
