440cded6a5037481ef3dff378ab332f3b7a15539
s261-08d <s261-08d@ba42aba2-8e26-4a4c-85cc-ac5429f0f81b>
2017-03-05 19:43:52 +0000

    login screen shows up now -josh
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@29 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- HealthNet/urls.py
+++ HealthNet/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
+    #url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 	url(r'^', include('Profile.urls')),
 	url(r'^', include('Calendar.urls')),
+    url(r'^', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
 ]
