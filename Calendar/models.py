6f812aba6162588623c5254c647570a90668a3d9
s261-08d <s261-08d@ba42aba2-8e26-4a4c-85cc-ac5429f0f81b>
2017-03-05 21:35:25 +0000

    scheduler first implementation -evan
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@30 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- Calendar/models.py
+++ Calendar/models.py
@@ -1,32 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import Appointment
-from datetime import datetime
+
 # Create your models here.
-
-class Event(models.Model):
-    title = models.CharField(u"Title", max_length=50)
-    description = MarkdownTextField(u"D")
-    comments = models.TextField(u"Comentarios")
-    date = models.DateField()
-    starts = models.TimeField(blank=True, null=True)
-    ends = models.TimeField(blank=True, null=True)
-    price = models.CharField(default="Entrada gratuita", max_length=50)
-    venue = models.ForeignKey(Venue)
-    category = models.ForeignKey(Category)
-    picture = ResizedImageField(upload_to = upload_to_event, 
-                                max_width=185, max_height=185, 
-                                blank=True, null=True)
-    
-    status = models.IntegerField()
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User)
-    
-    class Meta:
-        verbose_name = u"Evento"
-        verbose_name_plural = u"Eventos"
-        ordering = ['date','starts']
-    
-    def __unicode__(self):
-        return self.title
-
--- Calendar/views.py
+++ Calendar/views.py
@@ -1,60 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-def named_month(month_number):
-
-	"""
-	Return the name of the month, given the number.
-	"""
-	return date(1900, month_number, 1).strftime("%B")
-
-def this_month(request):
-	"""
-	Show calendar of readings this month.
-	"""
-	today = datetime.now()
-	return calendar(request, today.year, today.month)
-
-
-def calendar(request, year, month, series_id=None):
-	"""
-	Show calendar of readings for a given month of a given year.
-	``series_id``
-	The reading series to show. None shows all reading series.
-	"""
-
-	my_year = int(year)
-	my_month = int(month)
-	my_calendar_from_month = datetime(my_year, my_month, 1)
-	my_calendar_to_month = datetime(my_year, my_month, monthrange(my_year, my_month)[1])
-
-	my_reading_events = Reading.objects.filter(date_and_time__gte=my_calendar_from_month).filter(date_and_time__lte=my_calendar_to_month)
-	if series_id:
-		my_reading_events = my_reading_events.filter(series=series_id)
-
-	# Calculate values for the calendar controls. 1-indexed (Jan = 1)
-	my_previous_year = my_year
-	my_previous_month = my_month - 1
-	if my_previous_month == 0:
-		my_previous_year = my_year - 1
-		my_previous_month = 12
-	my_next_year = my_year
-	my_next_month = my_month + 1
-	if my_next_month == 13:
-		my_next_year = my_year + 1
-		my_next_month = 1
-	my_year_after_this = my_year + 1
-	my_year_before_this = my_year - 1
-	return render_to_response("cal_template.html", { 'readings_list': my_reading_events,
-														'month': my_month,
-														'month_name': named_month(my_month),
-														'year': my_year,
-														'previous_month': my_previous_month,
-														'previous_month_name': named_month(my_previous_month),
-														'previous_year': my_previous_year,
-														'next_month': my_next_month,
-														'next_month_name': named_month(my_next_month),
-														'next_year': my_next_year,
-														'year_before_this': my_year_before_this,
-														'year_after_this': my_year_after_this,
-	}, context_instance=RequestContext(request))
+def index(request, calendar_slug):
+    try:
+        schedule = schedule.objects.get(pk=schedule)
+    except Question.DoesNotExist:
+        raise Http404("The requested calendar does not exist")
+    return render(request, 'schedule/calendar.html', {'calendar_slug': calendar_slug})
