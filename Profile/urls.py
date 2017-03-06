3676e37ba5caf56899ddf0abcea29be3f940d2c2
s261-08d <s261-08d@ba42aba2-8e26-4a4c-85cc-ac5429f0f81b>
2017-03-06 03:58:12 +0000

    login and logout works.--Yuying
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@38 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- Profile/urls.py
+++ Profile/urls.py
@@ -3,8 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-	url(r'^', views.home, name='home'),
-	url(r'^login/$',views.login_page),
+	url(r'^$', views.home, name='home'),
 	url(r'^logout/$', views.logout_page, name='logout'),
 	url(r'^register/$', views.register, name='register')
 ]
