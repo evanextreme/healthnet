18588894de59f1604a2d427454dcd00c2d355d39
s261-08d <s261-08d@ba42aba2-8e26-4a4c-85cc-ac5429f0f81b>
2017-03-06 12:16:06 +0000

    login, logout, register are all fully functional -josh
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@40 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- Profile/forms.py
+++ Profile/forms.py
@@ -9,21 +9,21 @@ class RegistrationForm(forms.Form):
     password1=forms.CharField(label='Password',widget=forms.PasswordInput())
     password2=forms.CharField(label='Password(Again)',widget=forms.PasswordInput())
     
-def clean_password2(self):
-    if 'password1' in self.cleaned_data:
-        password1=self.cleaned_data['password1']
-        password2=self.cleaned_data['password2']
-        if password1==password2:
-            return password2
-    raise forms.ValidationError('Passwords do not match.')
+    def clean_password2(self):
+        if 'password1' in self.cleaned_data:
+            password1=self.cleaned_data['password1']
+            password2=self.cleaned_data['password2']
+            if password1==password2:
+                return password2
+        raise forms.ValidationError('Passwords do not match.')
 
-def clean_username(self):
-    username=self.cleaned_data['username']
-    if not re.search(r'^\w+$',username):
-        raise forms.ValidationError('Username can only contain alphanumeric characters and underscore.')
+    def clean_username(self):
+        username=self.cleaned_data['username']
+        if not re.search(r'^\w+$',username):
+            raise forms.ValidationError('Username can only contain alphanumeric characters and underscore.')
 
-    try:
-        User.objects.get(username=username)
-    except ObjectDoesNotExist:
-        return username
-    raise forms.ValidationError('Username is already taken.')
+        try:
+            User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            return username
+        raise forms.ValidationError('Username is already taken.')
