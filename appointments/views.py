64a0d776c46f04aac1a46c11ffa115fd192fc2e5
s261-08d <s261-08d@ba42aba2-8e26-4a4c-85cc-ac5429f0f81b>
2017-03-06 16:55:47 +0000

    Added basic patient/doctor/hospital to the database
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@42 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- appointments/views.py
+++ appointments/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from HealthNet import models
-import datetime
-
+from HealthNet.models import Appointment
+from datetime import date, time, datetime
+from django.utils import timezone
 
 def new_appt(request):
     patients = models.Patient.patients.order_by('first_name')
@@ -17,9 +17,10 @@ def create(request):
         doctor = request.POST.get('doctor')
         appointment_type = request.POST.get('appointment_type')
         date = request.POST.get('date')
+        print(date)
         time = request.POST.get('time')
-        dtime = datetime.combine(date, time)
-        A = Appointment(date=dtime,
+#        dtime = datetime.combine(date, time)
+        A = Appointment(date= timezone.now(),
                         patient=patient,
                         doctor=doctor,
                         nurse=doctor,
