49127b4ba3e074f111f517d353c46bc1c0e76720
s261-08d <s261-08d@ba42aba2-8e26-4a4c-85cc-ac5429f0f81b>
2017-03-06 19:27:19 +0000

    Appointment form works methinks -JAKE
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@45 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^new/$', views.new_appt, name='new_appt'),
-    url(r'^create/$',views.create, name='create'),
+    url(r'^create/$', views.create, name='create'),
 ]
