08f73d7738c2d3f4f169f6db2fc391b4572aeff3
Evan Hirsh <contact@ehirsh.com>
2017-03-20 21:56:13 +0000

    field now datetime
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@68 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- HealthNet/settings.py
+++ HealthNet/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'Calendar',
     'eventlog',
     'schedule',
-    'fullcalendar',
     'appointments',
 ]
 
--- Profile/forms.py
+++ Profile/forms.py
@@ -5,7 +5,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from HealthNet.models import Patient
 
 
-
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
@@ -13,6 +12,7 @@ class UserForm(forms.ModelForm):
         fields = ['username','first_name', 'last_name', 'email', 'password']
 
 class PatientForm(forms.ModelForm):
+    date_of_birth = forms.DateField(widget=forms.SelectDateWidget())
     class Meta:
         model = Patient
         fields = ['date_of_birth']
