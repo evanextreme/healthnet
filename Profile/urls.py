11bf67a8dcc09b6fcabb989dcbefdceda87cc321
Jacob VanScoy <jdv6718@rit.edu>
2017-03-23 21:05:41 +0000

    bootstrap djangodatetimewidget added in event creation
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@76 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- Profile/urls.py
+++ Profile/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
+	url(r'^appt/new/$', views.new_appt, name='new_appt'),
+	url(r'^appt/create/$', views.new_appt, name='new_appt'),
 	url(r'^login/$', auth_views.login, name='login'),
 	url(r'^logout/$', views.logout_page, name='logout'),
     url(r'^register/$', views.register_page,name='register'),
