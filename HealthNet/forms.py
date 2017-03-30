16d69f9330713ac0a737bf347b204bc5a5b22807
Evan Hirsh <evanx11@gmail.com>
2017-03-30 21:52:37 +0000

    refactor 2: electric boogaloo
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@88 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

---
+++ HealthNet/forms.py
@@ -0,0 +1,28 @@
+import re
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from HealthNet.models import Patient
+
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ['username','first_name', 'last_name', 'email', 'password']
+
+class PatientForm(forms.ModelForm):
+    date_of_birth = forms.DateField(widget=forms.SelectDateWidget())
+    class Meta:
+        model = Patient
+        fields = ['date_of_birth']
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['email','first_name','last_name']
