9eb4700e8f7d6a97880ff2705f76657b5eab51d9
Evan Hirsh <contact@ehirsh.com>
2017-04-06 22:32:14 +0000

    patient medical info updating
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@101 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- HealthNet/urls.py
+++ HealthNet/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 from . import signals, views
 
 
