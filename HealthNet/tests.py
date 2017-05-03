a91e5a58f89aa21bea0ee21a1925505c83d639d8
Evan Hirsh <evanx11@gmail.com>
2017-05-03 18:25:31 +0000

    oh, and tests.py
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@166 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

---
+++ HealthNet/tests.py
@@ -0,0 +1,143 @@
+from django.contrib.auth.models import User
+from django.utils import timezone
+from Calendar.models import *
+from .models import *
+from .email import appointment_confirmation_email
+from .console import print_status
+from django.core.mail import send_mail
+from .console import print_status
+from .settings import EMAIL_HOST_USER, DEBUG
+
+
+
+def initialize_database():
+    print_status('STATUS',str('Initializing test objects'))
+    try:
+        try:
+            patient_user = User.objects.create_user(username='test_patient', email='username@example.com', first_name="Patient",last_name="Test", password='qwertyuiop')
+            print_status('GOOD',str('Patient user created.'))
+            patient_user.save()
+        except Exception as error:
+            print_status('FAIL',str('Patient user initialization failed! Attempting to grab existing from DB'))
+            patient_user = User.objects.get(username='test_patient')
+
+        try:
+            nurse_user = User.objects.create_user(username='test_nurse', email='username@example.com', first_name="Nurse",last_name="Test", password='qwertyuiop')
+            print_status('GOOD',str('Nurse user created.'))
+            nurse_user.save()
+        except Exception as error:
+            print_status('FAIL',str('Nurse user initialization failed!'))
+            nurse_user = User.objects.get(username='test_nurse')
+
+        try:
+            doctor_user = User.objects.create_user(username='test_doctor', email='username@example.com', first_name="Doctor",last_name="Test", password='qwertyuiop')
+            print_status('GOOD',str('Doctor user created.'))
+            doctor_user.save()
+        except Exception as error:
+            doctor_user = User.objects.get(username='test_patient')
+            print_status('FAIL',str('Doctor user initialization failed!'))
+
+        try:
+            test_hospital = Hospital.hospitals.create(name="Test Hospital",address="123 Test Street")
+            print_status('GOOD',str('Hospital object created and linked.'))
+            test_hospital.save()
+        except Exception as error:
+            print_status('FAIL',str('Hospital object initialization failed!'))
+            raise error
+
+        try:
+            test_doctor = Doctor.doctors.create(user=doctor_user,phone_number='+1234568900')
+            test_doctor.hospital.add(test_hospital)
+            print_status('GOOD',str('Hospital object added to doctor.'))
+            test_doctor.save()
+            print_status('GOOD',str('Doctor object created and saved to database.'))
+        except Exception as error:
+            print_status('FAIL',str('Doctor object initialization failed!'))
+            raise error
+
+        try:
+            test_nurse = Nurse.nurses.create(user=nurse_user,phone_number='+12345678900',hospital=test_hospital)
+            test_nurse.save()
+            print_status('GOOD',str('Nurse object created and linked.'))
+        except Exception as error:
+            print_status('FAIL',str('Nurse object initialization failed!'))
+            raise error
+
+        try:
+            test_patient = Patient.patients.create(user=patient_user,phone_number='+12345678900',height=100,weight=100,doctor=test_doctor, hospital=test_hospital)
+            test_patient.save()
+            print_status('GOOD',str('Patient object created and linked.'))
+        except Exception as error:
+            print_status('FAIL',str('Patient object initialization failed!'))
+            raise error
+
+        try:
+            test_appointment = CalendarEvent.appointments.create(title='test_appointment',start=timezone.now(),end=timezone.now(), patient=test_patient,doctor=test_doctor,hospital=test_hospital)
+            test_appointment.save()
+            print_status('GOOD',str('Appointment object created, linked, and saved.'))
+        except Exception as error:
+            print_status('FAIL',str('Appointment object initialization failed!'))
+            raise error
+
+    except Exception as error:
+        print_status('DATA',str(error))
+        raise error
+
+
+
+def email_test():
+    print_status('STATUS',str('Please input an email address to act as a recipient for testing'))
+    TEST_EMAIL = input('Email: ')
+    patient = User.objects.get(username='test_patient').patient
+    doctor = User.objects.get(username='test_doctor').doctor
+    patient.user.email = TEST_EMAIL
+    patient.user.save()
+    doctor.user.email = TEST_EMAIL
+    doctor.user.save()
+    appointment = CalendarEvent.appointments.get(title='test_appointment')
+    print_status('STAT',str('Sending test email, please check your inbox.'))
+    try:
+        subject = 'TEST EMAIL PLEASE IGNORE'.format(doctor.user.last_name)
+        message = """Hi {}! We're letting you know that your appointment with Doctor {} has been confirmed. Here are the details: \n
+The appointment '{}' is scheduled for {}, at your hospital, {}. If you would like to reschedule, please contact your doctor, at {}. Do NOT reply to this email. Thank you, and have a good day!
+""".format(patient.user.first_name, doctor.user.last_name, appointment.title, appointment.start, patient.hospital, doctor.user.email)
+
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[str(patient.user.email),str(doctor.user.email)],
+            fail_silently=False,
+        )
+        print_status('GOOD',str("""Appointment confirmation email to Patient '{} {}' and Doctor '{} {}' about appointment '{}' sent!""".format(patient.user.first_name,patient.user.last_name,doctor.user.first_name,doctor.user.last_name,appointment.title)))
+    except Exception as error:
+        print_status('WARN',str('Appointment confirmation email to Patient {} {} and Doctor {} {} failed! This is likely because your SMTP settings are incorrect, or the email entered as a test recipient was incorrect. If it is the former, email functionality will not work without proper settings set in HealthNet/settings.py'.format(patient.user.first_name,patient.user.last_name,doctor.user.first_name,doctor.user.last_name)))
+        print(error)
+
+def delete_test_objects():
+    print_status('STATUS',str('Cleanup of database artifacts starting!'))
+    appointments = User.objects.get(username='test_doctor').doctor.calendarevent_set.all()
+    for appointment in appointments:
+        print_status('DATA',str("""Deleting Appointment ID '{}'""").format(appointment.appointment_id))
+        appointment.delete()
+
+    User.objects.get(username='test_patient').patient.delete()
+    print_status('GOOD',str('Patient object deleted'))
+    User.objects.get(username='test_nurse').nurse.delete()
+    print_status('GOOD',str('Nurse object deleted'))
+    User.objects.get(username='test_doctor').doctor.delete()
+    print_status('GOOD',str('Doctor object deleted'))
+
+
+    User.objects.get(username='test_patient').delete()
+    print_status('GOOD',str('Patient user deleted'))
+    User.objects.get(username='test_nurse').delete()
+    print_status('GOOD',str('Nurse user deleted'))
+    User.objects.get(username='test_doctor').delete()
+    print_status('GOOD',str('Doctor user deleted'))
+
+def settings_warn():
+    if(DEBUG == True):
+        print_status('STATUS','Debug enviornment currently enabled.')
+    if(EMAIL_HOST_USER == 'myhealthnoreply@gmail.com'):
+        print_status('HARDWARN','Your EMAIL_HOST_USER enviornment variable in settings.py has NOT been changed from the default. It is HIGHLY reccomended that you change this variable before running this system in a production enviornment')
