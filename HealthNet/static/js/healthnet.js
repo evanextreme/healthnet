4005f1d11f58eddf000a0815f90868665ff40708
Evan Hirsh <evanx11@gmail.com>
2017-05-04 21:45:29 +0000

    try this
    git-svn-id: svn+ssh://control.se.rit.edu/home/teams/261/s261-08d/svn/trunk@171 ba42aba2-8e26-4a4c-85cc-ac5429f0f81b

--- HealthNet/static/js/healthnet.js
+++ HealthNet/static/js/healthnet.js
@@ -135,6 +135,41 @@ function downloadAttachment(attachmenturl){
   }
 }
 
+function updateAppointment(get_appointment_id) {
+      $.ajaxSetup({
+          beforeSend: function(xhr, settings) {
+              function getCookie(name) {
+                  var cookieValue = null;
+                  if (document.cookie && document.cookie != '') {
+                      var cookies = document.cookie.split(';');
+                      for (var i = 0; i < cookies.length; i++) {
+                          var cookie = jQuery.trim(cookies[i]);
+                          // Does this cookie string begin with the name we want?
+                          if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                              break;
+                          }
+                      }
+                  }
+                  return cookieValue;
+              }
+              if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+                  // Only send the token to relative URLs i.e. locally.
+                  xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+              }
+          }
+      });
+    $.ajax({
+        type: 'POST',
+        url: '.',
+        data: { appointmentId: get_appointment_id },
+        success: function(response) {
+          $('#apt-div').html(response);
+        }
+    });
+    $('#apt-modal').modal('open');
+}
+
 function updatePatient(get_patient_id) {
       $.ajaxSetup({
           beforeSend: function(xhr, settings) {
